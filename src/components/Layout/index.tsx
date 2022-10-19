9bc3cd2e73437595425ea1dc0e9ee7b8ad023a61
jaewoong2 <jwisgenius@naver.com>
2022-10-19 16:22:58 +0900

    :lipstick: Footer 에 작성자 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,10 +5,12 @@ const Layout = ({ children, title }: PropsWithChildren<{ title?: React.ReactNode
     <div className="bg-gray-100 min-h-screen w-full flex justify-center items-center flex-col">
       {title}
       <div className="min-h-[400px] max-w-[500px] w-full bg-gray-300 shadow-lg">{children}</div>
-      <footer className="w-[500px] flex justify-end py-2">
+      <footer className="w-[500px] flex items-end py-2 text-xs text-gray-600 flex-col">
         <a target="_blank" href="https://github.com/jaewoong2/22-graduation" rel="noreferrer">
-          2022-2 @jaewoong2
+          2022-2 전자정보공학부 IT융합전공 졸업작품
         </a>
+        <p>20160569 임재웅</p>
+        <p>20160561 이창범</p>
       </footer>
     </div>
   )
