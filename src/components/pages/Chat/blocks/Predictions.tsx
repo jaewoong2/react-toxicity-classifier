077df1357e5f047c128a23439ab6ec22911d171f
jaewoong2 <jwisgenius@naver.com>
2022-10-19 16:24:07 +0900

    :lipstick: 모달창에 영어 문장 추가

--- src/components/pages/Chat/blocks/Predictions.tsx
+++ src/components/pages/Chat/blocks/Predictions.tsx
@@ -3,19 +3,28 @@ import { getKoreanLabel } from '@/utils'
 import useChatValueContext from '../hooks/useChatValueContext'
 
 const Predictions = ({ index }: { index: number }) => {
-  const { messages } = useChatValueContext()
-
+  const { messages, messageCache } = useChatValueContext()
   return (
-    <div className="grid grid-cols-2 gap-2">
-      {messages?.[index]?.prediction?.map((predict) => {
-        return (
-          <p className="border rounded-xl p-2 text-sm hover:bg-gray-200 hover:text-gray-700">
-            {getKoreanLabel(predict.label)}
-            {` - ${predict.results.map((v) => (v.probabilities[1] * 100).toString().slice(0, 4))}%`}
-          </p>
-        )
-      })}
-    </div>
+    <>
+      <h2 className="text-sm pb-4 italic">
+        {messages?.[index]?.message} ({messageCache?.get(messages?.[index]?.message)})
+      </h2>
+      <div className="grid grid-cols-2 gap-2">
+        {messages?.[index]?.prediction?.map((predict) => {
+          return (
+            <p
+              key={predict.label + index}
+              className="border rounded-xl p-2 text-sm hover:bg-gray-200 hover:text-gray-700"
+            >
+              {getKoreanLabel(predict.label)}
+              {` - ${predict.results.map((v) =>
+                (v.probabilities[1] * 100).toString().slice(0, 4)
+              )}%`}
+            </p>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
